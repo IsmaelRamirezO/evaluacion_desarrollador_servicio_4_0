001a41c6f0f3a03b7e6c8a6fb9dce6a90edeeabd
IsmaelRamirezO <ismael.ramirez@dportenis.com.mx>
2023-08-16 09:00:37 -0700

    Actualizacion en Dev1
    Cambio de prueba.

--- modelo/api.php
+++ modelo/api.php
@@ -43,7 +43,7 @@
 				$ObjetoColor = new objeto();				
 				$Validar = new valida();
 				$Valor = [];
-				
+				//Cambios Dev1
 				$Validar->CreaRespuesta("0", "", $Valor);
 				
 				echo json_encode($Validar->ObtenerResponse(), JSON_PRETTY_PRINT  | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
